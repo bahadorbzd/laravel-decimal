ddb4965b6c004b76b5c06c3822d29049e2118511
bahadorbzd <email.bahador@gmail.com>
2022-12-07 02:57:02 +0330

    Update DecimalObjectCast.php

--- src/DecimalObjectCast.php
+++ src/DecimalObjectCast.php
@@ -7,13 +7,27 @@ use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
 class DecimalObjectCast implements CastsAttributes
 {
+    private int $places;
+
+    private int $precision;
+
+    /**
+     * @param int $places
+     * @param int $precision
+     */
+    public function __construct(int $places = 0, int $precision = Decimal::DEFAULT_PRECISION)
+    {
+        $this->places = $places;
+        $this->precision = $precision;
+    }
+
     public function set($model, string $key, $value, array $attributes)
     {
-        return $value->toFixed();
+        return $value->toFixed($this->places);
     }
 
     public function get($model, string $key, $value, array $attributes)
     {
-        return new Decimal($value);
+        return new Decimal($value, $this->precision);
     }
 }
